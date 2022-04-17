7c95c2442071951fafcaaa50fd105c2e045f9cca
SunnyYadav16 <180120107188@git.org.in>
2022-04-17 23:14:02 +0530

    Updated code
    Code with searching capabilities included into it

--- flask_learning/__init__.py
+++ flask_learning/__init__.py
@@ -1,10 +1,8 @@
-import flask_sijax
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-from flask_sijax import Sijax
 
 
 from flask_learning.config import Config
@@ -15,7 +13,6 @@ bcrypt = Bcrypt()
 login_manager = LoginManager()
 login_manager.login_view = 'users.user_login'
 login_manager.login_message_category = 'info'
-sijax = flask_sijax.Sijax()
 mail = Mail()
 
 
@@ -27,7 +24,6 @@ def create_app(config_class=Config):
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
-    sijax.init_app(app)
 
     from flask_learning.users.routes import users
     from flask_learning.posts.routes import posts
--- flask_learning/main/routes.py
+++ flask_learning/main/routes.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, request, render_template
 from flask_learning.models import Post
+from flask_learning.posts.forms import SearchForm
+from sqlalchemy import or_
+
 
 main = Blueprint('main', __name__)
 
@@ -15,3 +18,24 @@ def home_page():
 @main.route("/about")
 def about_user():
     return render_template('about.html', title='About')
+
+
+@main.context_processor
+def navbar():
+    form = SearchForm()
+    return dict(form=form)
+
+@main.route('/search', methods=['POST'])
+def search_post():
+    form = SearchForm()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=2)
+    if form.validate_on_submit():
+
+        post_searched = form.searched.data
+        print(post_searched)
+        posts = posts.filter(or_(Post.title.like('%' + post_searched + '%'),Post.content.like('%' + post_searched + '%')))
+        posts = posts.order_by(Post.title).all()
+
+
+    return render_template("search.html", searched=post_searched, form=form, posts=posts)
