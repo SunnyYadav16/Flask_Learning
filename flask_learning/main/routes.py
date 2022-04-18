895bf6c8aab8ed0c8e16e99e9f10b97ef2cd54e5
SunnyYadav16 <180120107188@git.org.in>
2022-04-18 10:00:45 +0530

    changes

--- flask_learning/main/routes.py
+++ flask_learning/main/routes.py
@@ -1,8 +1,7 @@
 from flask import Blueprint, request, render_template
-from flask_learning.models import Post
+from flask_learning.models import Post, User
 from flask_learning.posts.forms import SearchForm
 from sqlalchemy import or_
-
 
 main = Blueprint('main', __name__)
 
@@ -25,16 +24,18 @@ def navbar():
     form = SearchForm()
     return dict(form=form)
 
+
 @main.route('/search', methods=['POST'])
 def search_post():
     form = SearchForm()
-    page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=2)
+    # page = request.args.get('page', 1, type=int)
+    posts = Post.query
+    # posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=2)
     if form.validate_on_submit():
 
         post_searched = form.searched.data
-        print(post_searched)
-        posts = posts.filter(or_(Post.title.like('%' + post_searched + '%'),Post.content.like('%' + post_searched + '%')))
+
+        posts = posts.filter(or_(Post.title.like('%' + post_searched + '%'), Post.content.like('%' + post_searched + '%'), User.username == post_searched ))
         posts = posts.order_by(Post.title).all()
 
 
